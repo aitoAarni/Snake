6142ff6213b522dede8f414b573fb1ad7ba1298e
aitoAarni <aarni.rechardt@gmail.com>
2026-03-25 22:10:30 +0200

    add block movement

--- src/block.cpp
+++ src/block.cpp
@@ -5,3 +5,11 @@ Block::Block(int x, int y, char sym) : x_pos(x), y_pos(y), symbol(sym) {}
 int Block::get_x() const { return x_pos;}
 int Block::get_y() const { return y_pos;}
 char Block::get_symbol() const { return symbol;}
+
+SnakeBlock::SnakeBlock(int x, int y, char sym, Direction dir) :
+    Block(x, y, sym), direction(dir) {}
+
+void SnakeBlock::move_rel(int x, int y) {
+    x_pos += x;
+    y_pos += y;
+}
--- src/block.hpp
+++ src/block.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include "controls.hpp"
 
 class Block {
+    protected:
     int x_pos;
     int y_pos;
     char symbol;
@@ -9,4 +11,14 @@ class Block {
     int get_x() const;
     int get_y() const;
     char get_symbol() const;
+};
+
+class SnakeBlock : public Block {
+    Direction direction;
+    public:
+    SnakeBlock(int, int, char, Direction);
+    Direction get_direction() const {
+        return direction;
+    }
+    void move_rel(int x, int y);
 };
